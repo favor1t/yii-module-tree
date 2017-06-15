151653648807bce91f33f493643dd27d99ff85a1
favor1t <favor1t@yandex.ru>
2017-06-15 22:01:05 +0300

    TreeList

--- tree/common/TreeList.php
+++ tree/common/TreeList.php
@@ -16,27 +16,35 @@ class TreeList extends Tree
     static function getTreeList()
     {
         $res = parent::find()
-            ->indexBy('id')
-            ->orderBy(['sid' => SORT_ASC])
+            ->indexBy['id']
+            ->orderBy(['pid' => SORT_ASC])
             ->asArray()
             ->all();
 
-        return self::_prepareTreeList($res);
-
+        return self::_createTreeList($res, 'pid');
     }
 
-    private static function _prepareTreeList($res)
+    private static function _createTreeList($res, $key)
     {
         if (is_array($res)) {
             foreach ($res as $id => $node) {
-                if (isset($res[$node['pid']])) {
-                    $res[$node['pid']]['sub'][$id] = &$res[$id];
-                }
+                if (isset($res[$node[$key]]))
+                    $res[$node[$key]]['sub'][$id] = &$res[$id];
             }
-            return $res;
+            return self::_prepareTreeList($res, $key, 0);
         }
 
         return false;
+    }
+
+    private static function _prepareTreeList($res, $key, $val)
+    {
+        foreach ($res as $id => $node) {
+            if ($node[$key] != $val)
+                unset($res[$id]);
+        }
+
+        return $res;
     }
 
 }
